61d5a23853d0f9e0f780d75f922db659bc1e9464
rudolfsaditajs <rudolfs.aditajs@gmail.com>
2020-10-20 17:31:38 +0300

    day 2

--- SEF/src/sef/module4/activity/Calculator.java
+++ SEF/src/sef/module4/activity/Calculator.java
@@ -9,12 +9,16 @@ package sef.module4.activity;
  */
 
 
-	 
+
 
 public class Calculator {
+	
+	public static void main(String[] args) {
+		
+	}
 
 	
-	private String calculatorType;
+	public String calculatorType;
 	public static final String CALC_TYPE_BASIC = "Basic";
 	
 	// Default constructor
@@ -36,13 +40,13 @@ public class Calculator {
 	}
 	
 	// Add 
-	public int add(object ;) {
+	public static int add(int x, int y) {
 		int sum = x + y;
 		return sum;
 	}
 
 	// Subtract
-	public int subtract(int x, int y) {
+	public static int subtract(int x, int y) {
 		int diff = 0;
 		if (x > y) {
 			diff = x - y;
@@ -54,7 +58,7 @@ public class Calculator {
 	}
 
 	// Multiply
-	public int multiply(int[] numbers) {
+	public static int multiply(int[] numbers) {
 		int temp = 1;
 
 		for (int i = 0; i < numbers.length; i++) {
@@ -65,7 +69,7 @@ public class Calculator {
 	}
 
 	// Divide
-	public int divide(int x, int y) {
+	public static int divide(int x, int y) {
 		int divValue = 0;
 		if (x == 0 || y == 0) {
 			divValue = 0;
